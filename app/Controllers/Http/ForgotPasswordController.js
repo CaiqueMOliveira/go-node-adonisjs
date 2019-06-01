61ead1a768e6c7001befef292e1e495b9a0fde1f
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2019-06-01 15:02:36 -0300

    create reset password functionality

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const moment = require('moment')
 const crypto = require('crypto')
 const User = use('App/Models/User')
 const Mail = use('Mail')
@@ -35,6 +36,39 @@ class ForgotPasswordController {
           message:
             'Something went wrong! Is there an user with this email address?'
         }
+      })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const foundUser = await User.findBy('token', token)
+
+      if (!foundUser) {
+        return response.status(404).send({ message: 'invalid token' })
+      }
+
+      const tokenHasBeenExpired = moment()
+        .subtract(2, 'days')
+        .isAfter(foundUser.token_created_at)
+
+      if (tokenHasBeenExpired) {
+        return response
+          .status(401)
+          .send({ message: 'The informed token has been expired' })
+      }
+
+      foundUser.token = null
+      foundUser.token_created_at = null
+      foundUser.password = password
+
+      await foundUser.save()
+    } catch (error) {
+      return response.status(error.status).send({
+        message: 'Something went wrong while resetting your password!',
+        rawError: error.message
       })
     }
   }
--- start/routes.js
+++ start/routes.js
@@ -3,5 +3,8 @@
 const Route = use('Route')
 
 Route.post('users', 'UserController.store')
+
 Route.post('session', 'SessionController.store')
+
 Route.post('password', 'ForgotPasswordController.store')
+Route.put('password', 'ForgotPasswordController.update')
