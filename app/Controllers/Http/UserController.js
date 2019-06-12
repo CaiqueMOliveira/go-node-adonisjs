f87217c6ef0d4567a6c00b17864c5cb662a5cfef
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2019-06-12 10:42:49 -0300

    create address relationship to user

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -15,7 +15,11 @@ class UserController {
       'password'
     ])
 
-    const newCreatedUser = await User.creaste(userDataForInsertingNewOne)
+    const newCreatedUser = await User.create(userDataForInsertingNewOne)
+
+    const addresses = request.input('addresses')
+
+    await newCreatedUser.addresses().createMany(addresses)
 
     return newCreatedUser
   }
